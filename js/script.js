81186a4cecdf27eb672172a88079f87d21c6011a
HiFuruya <h.furuya@hotmail.com>
2022-08-25 22:08:32 -0300

    Projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const tubo = document.querySelector('.tubo');
 
 const pulo = () =>{
     mario.classList.add('pulo');
@@ -9,3 +10,25 @@ const pulo = () =>{
 };
 
 document.addEventListener('keydown', pulo);
+
+
+const loop = setInterval(()=> {
+
+    const tuboPosition = tubo.offsetLeft;
+
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (tuboPosition <= 124 && marioPosition <= 100 && tuboPosition > 0) {
+        tubo.style.animation = 'none';
+        tubo.style.left = `${tuboPosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "images/game-over.png";
+        mario.style.width = '80px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+}, 10);
